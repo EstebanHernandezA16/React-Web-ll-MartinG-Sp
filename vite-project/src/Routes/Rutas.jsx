278d9130fa0de8362e087681b727a7adfcef16d9
deivy-0 <131556021+deivy-0@users.noreply.github.com>
2023-04-26 08:15:52 -0500

    Se agregan entregables en Members

--- vite-project/src/Routes/Rutas.jsx
+++ vite-project/src/Routes/Rutas.jsx
@@ -4,6 +4,7 @@ import { Home } from "../Home/Home";
 import { About } from "../About/About";
 import { Menu } from "../shared/Menu/Menu";
 import { Footer } from "../shared/Footer/Footer";
+import { Members } from "../Members/Members";
 // 
 export const Rutas = () => {
   return (
@@ -16,6 +17,7 @@ export const Rutas = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         {/* <Route path="/api" element={<Api/>}/> */}
+        <Route path="/m" element={<Members/>}/>
       </Routes>
 
       <div style={{ width: '100%', height: '300px', backgroundColor: '#090D0E'}}>
